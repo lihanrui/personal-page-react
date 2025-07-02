c0597980d1b95b2840e5fab1b6a9cf444d0f7b74
Hanrui Li <lihanrui@gmail.com>
2025-07-01 22:19:55 -0700

    Remove github icon

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { useThemeContext } from '../contexts/ThemeContext';
-import { ReactComponent as GithubIcon } from '../assets/github.svg';
 
 const footerStyle = css`
   padding: 40px 16px;
@@ -12,18 +11,7 @@ const footerStyle = css`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
-    svg {
-      width: 2rem;
-      height: 2rem;
-    }
   }
-`;
-
-const iconStyle = (color) => css`
-  width: 2rem;
-  height: 2rem;
-  stroke: ${color};
 `;
 
 const Footer = () => {
@@ -33,11 +21,6 @@ const Footer = () => {
     <footer css={[footerStyle]}>
       <nav>
         <div>{new Date().getFullYear()} &copy; your copyright</div>
-        <div>
-          <a href="https://wikipedia.org" rel="noopener noreferrer" target="_blank">
-            <GithubIcon css={iconStyle(isLight ? '#000' : '#fff')} className="github" />
-          </a>
-        </div>
       </nav>
     </footer>
   );
